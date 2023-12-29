936053f09f7928498eb0ddbf68c9b68ced5535b9
gilpgdm <gilpgdm@gmail.com>
2023-12-29 14:54:20 -0600

    sw 4.11

--- docs/sw.js
+++ docs/sw.js
@@ -1,7 +1,7 @@
 /* Espera 11 minutos después de hacer los cambios en tu sitio, para depués
  * actualizar este archivo. */
 
-const VERSION = "4.10"
+const VERSION = "4.11"
 
 const CACHE = "cache"
 
