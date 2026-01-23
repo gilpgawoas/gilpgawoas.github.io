22857a867a1610bc03463eca4300f1f29f0f3816
gilpgdm <gilpgdm@gmail.com>
2026-01-23 01:47:59 -0600

    sw 6.27

--- docs/sw.js
+++ docs/sw.js
@@ -1,7 +1,7 @@
 /* Espera 11 minutos después de hacer los cambios en tu sitio, para depués
  * actualizar este archivo. */
 
-const VERSION = "6.26"
+const VERSION = "6.27"
 
 const CACHE = "cache"
 
